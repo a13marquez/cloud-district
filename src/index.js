5c325fa36368a4182240f42abecfac778338da9b
Álvaro Márquez <marquezalvaro13@gmail.com>
2020-08-14 10:10:26 +0200

    Add store to entry file

--- src/index.js
+++ src/index.js
@@ -3,10 +3,15 @@ import ReactDOM from 'react-dom'
 import './index.scss'
 import App from './containers/App'
 import * as serviceWorker from './serviceWorker'
+import { Provider } from 'react-redux'
+
+const store = configureStore()
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <Provider store={store}>
+      <App />
+    </Provider>
   </React.StrictMode>,
   document.getElementById('root')
 )
