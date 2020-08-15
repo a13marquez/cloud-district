9eccb80eaa57f5c7cc8ee7bd13b86e3bb1d9c925
Álvaro Márquez <marquezalvaro13@gmail.com>
2020-08-15 10:55:44 +0200

    Add Google initialization to app

--- src/containers/App.js
+++ src/containers/App.js
@@ -1,9 +1,34 @@
-import React from 'react'
+import React, { useEffect } from 'react'
+import PropTypes from 'prop-types'
+import { connect } from 'react-redux'
+import * as actions from '../store/actions/socialLoginActions'
 
-import './App.css'
+const googleClientId =
+  '235504936120-gfbge9o62rcq8hmo1p0g99vm4ka6gd0t.apps.googleusercontent.com'
 
-function App() {
-  return <div className="App"></div>
+function AppContainer({ initGoogle }) {
+  useEffect(() => {
+    initGoogle()
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, [])
+  return (
+    <div className="App">
+      <div> </div>{' '}
+    </div>
+  )
 }
 
-export default App
+AppContainer.propTypes = {
+  initGoogle: PropTypes.func.isRequired,
+}
+
+const mapDispatchToProps = (dispatch) => ({
+  initGoogle: () =>
+    dispatch(
+      actions.authServiceLoad('google', {
+        clientId: googleClientId,
+      })
+    ),
+})
+
+export default connect(null, mapDispatchToProps)(AppContainer)
--- src/index.js
+++ src/index.js
@@ -4,6 +4,7 @@ import './index.scss'
 import App from './containers/App'
 import * as serviceWorker from './serviceWorker'
 import { Provider } from 'react-redux'
+import configureStore from './store/configureStore'
 
 const store = configureStore()
 
@@ -19,4 +20,4 @@ ReactDOM.render(
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
 // Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
+serviceWorker.unregister()
