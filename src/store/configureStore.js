8f82ae71c4505e08d6cd666a4bf963599b651375
Álvaro Márquez <marquezalvaro13@gmail.com>
2020-08-15 10:55:24 +0200

    Fix store configuration

--- src/store/configureStore.js
+++ src/store/configureStore.js
@@ -1,4 +1,4 @@
-import { createStore, applyMiddleware, Middleware } from 'redux'
+import { createStore, applyMiddleware } from 'redux'
 import { createLogger } from 'redux-logger'
 import createSagaMiddleware from 'redux-saga'
 import sagas from './sagas/sagas'
@@ -7,10 +7,13 @@ import reducers from './reducers'
 
 const sagaMiddleware = createSagaMiddleware()
 export const configureStore = () => {
-  middlewares = sagaMiddleware[]
+  const middlewares = [sagaMiddleware]
   if (process.env.NODE_ENV !== 'production') {
     middlewares.push(createLogger())
   }
+  const store = createStore(reducers, applyMiddleware(...middlewares))
   sagaMiddleware.run(sagas)
-  return createStore(reducers, applyMiddleware(...middlewares))
+  return store
 }
+
+export default configureStore
