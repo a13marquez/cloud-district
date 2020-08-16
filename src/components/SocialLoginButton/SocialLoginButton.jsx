47910df54fc7d56f49e7b651c6e748e08916aea9
Álvaro Márquez <marquezalvaro13@gmail.com>
2020-08-16 17:47:01 +0200

    Update socialLoginButton

--- src/components/SocialLoginButton/SocialLoginButton.jsx
+++ src/components/SocialLoginButton/SocialLoginButton.jsx
@@ -11,6 +11,9 @@ export const StyledSocialLoginButton = styled.button`
   border-radius: 100px;
   border-color: #5c5c5c;
   color: #5c5c5c;
+  width: 100%;
+  background-color: #fff
+
   :hover {
     border-color: #2c2c2c;
     color: #2c2c2c;
@@ -21,6 +24,18 @@ export const StyledSocialLoginButton = styled.button`
     margin-bottom: 20px;
     justify-content: center;
     align-items: center;
+    .button-img {
+      width: 24px;
+      height: 24px;
+      z-index: 1;
+      position: absolute;
+      left: 16px;
+    }
+    . button-text {
+      z-index: 1;
+      position: relative;
+      display: inline-block;
+    }
   }
   @media only screen and (max-width: 767px) {
     padding-left: 40px;
@@ -31,8 +46,8 @@ export const StyledSocialLoginButton = styled.button`
 
 const SocialLoginButton = ({ text, img, onClickCb }) => (
   <StyledSocialLoginButton onClick={onClickCb}>
-    <span className="button-text">{text}</span>
     <span className="button-img">{img}</span>
+    <span className="button-text">{text}</span>
   </StyledSocialLoginButton>
 )
 
