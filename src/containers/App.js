06106e42eec8f270cfa3bfff3379001c60197350
Álvaro Márquez <marquezalvaro13@gmail.com>
2020-08-16 17:48:06 +0200

    Update App

--- src/containers/App.js
+++ src/containers/App.js
@@ -1,36 +1,35 @@
 import React, { useEffect } from 'react'
 import PropTypes from 'prop-types'
-import { connect } from 'react-redux'
+import { useDispatch } from 'react-redux'
 import * as actions from '../store/actions/socialLoginActions'
 
 import LoginPage from './LoginPage/LoginPage'
 
-import { Switch, Route, Link, withRouter } from 'react-router-dom'
+import { Switch, Route } from 'react-router-dom'
 import PrivateRoute from './PrivateRouter/PrivateRoute'
-
-import { getUser } from '../store/reducers'
+import NavBar from './NavBar/NavBar'
 
 const googleClientId =
   '235504936120-gfbge9o62rcq8hmo1p0g99vm4ka6gd0t.apps.googleusercontent.com'
 
-function AppContainer({ initGoogle, user }) {
+function App() {
   useEffect(() => {
     initGoogle()
     // eslint-disable-next-line react-hooks/exhaustive-deps
   }, [])
+  const dispatch = useDispatch()
 
+  const initGoogle = () =>
+    dispatch(
+      actions.authServiceLoad('google', {
+        clientId: googleClientId,
+      })
+    )
   const Hello = () => <div>hola</div>
 
   return (
     <div className="App">
-      <ul>
-        <li>
-          <Link to="/login">Login</Link>
-        </li>
-        <li>
-          <Link to="/dashboard">Dashboard</Link>
-        </li>
-      </ul>
+      <NavBar />
       <Switch>
         <Route exact path={'/login'} component={LoginPage} />
         <PrivateRoute exact path={'/'}>
@@ -41,23 +40,8 @@ function AppContainer({ initGoogle, user }) {
   )
 }
 
-AppContainer.propTypes = {
+App.propTypes = {
   initGoogle: PropTypes.func.isRequired,
 }
 
-const mapStateToProps = (state) => ({
-  user: getUser(state),
-})
-
-const mapDispatchToProps = (dispatch) => ({
-  initGoogle: () =>
-    dispatch(
-      actions.authServiceLoad('google', {
-        clientId: googleClientId,
-      })
-    ),
-})
-
-export default withRouter(
-  connect(mapStateToProps, mapDispatchToProps)(AppContainer)
-)
+export default App
