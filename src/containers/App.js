d9986262eda61d3c50620e8ba4217b77b6552964
Álvaro Márquez <marquezalvaro13@gmail.com>
2020-08-17 18:21:15 +0200

    Add logged in user check

--- src/containers/App.js
+++ src/containers/App.js
@@ -4,16 +4,24 @@ import * as actions from '../store/actions/socialLoginActions'
 
 import LoginPage from './LoginPage/LoginPage'
 
-import { Switch, Route } from 'react-router-dom'
+import { Switch, Route, Redirect, useHistory } from 'react-router-dom'
 import PrivateRoute from './PrivateRouter/PrivateRoute'
 import NavBar from './NavBar/NavBar'
 import { Dashboard } from './Dashboard/Dashboard'
 
 import { GoogleClientIdContext } from '../index'
+import UserCard from './UserCard/UserCard'
 
 function App() {
+  const history = useHistory()
   useEffect(() => {
     initGoogle()
+    if (checkLoggedIn()) {
+      debugger
+      const { name, picture } = JSON.parse(sessionStorage.getItem('userEntity'))
+      dispatch(actions.socialLoginSuccess({ user: { name, picture } }))
+      history.push('/users')
+    }
     // eslint-disable-next-line react-hooks/exhaustive-deps
   }, [])
   const dispatch = useDispatch()
@@ -25,14 +33,26 @@ function App() {
       })
     )
 
+  const checkLoggedIn = () => {
+    if (sessionStorage.getItem('userEntity') == null) {
+      return false
+    }
+    return true
+  }
+
   return (
     <div className="App">
       <NavBar />
       <Switch>
-        <Route exact path={'/login'} component={LoginPage} />
-        <PrivateRoute exact path={'/'}>
+        <Route exact path="/login" component={LoginPage} />
+        <PrivateRoute exact path="/users">
           <Dashboard />
         </PrivateRoute>
+        <PrivateRoute exact path="/users/:id">
+          <UserCard />
+        </PrivateRoute>
+
+        <Redirect exact path="/" to="/users" />
       </Switch>
     </div>
   )
