47354969baf8285ef9a1901efb6b87ddaa14b3ce
Álvaro Márquez <marquezalvaro13@gmail.com>
2020-08-17 16:03:11 +0200

    Update users service

--- src/services/UsersService.js
+++ src/services/UsersService.js
@@ -4,4 +4,7 @@ export default {
   getUsersByPage(page) {
     return apiClient.get(`/users?per_page=5&page=${page}`)
   },
+  updateUserByID({ id, first_name, last_name, email }) {
+    return apiClient.patch(`/users/${id}`, { first_name, last_name, email })
+  },
 }
