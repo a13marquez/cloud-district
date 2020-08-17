dc640cf97102d613cd402caf10afab0ff7dfc30c
Álvaro Márquez <marquezalvaro13@gmail.com>
2020-08-17 16:02:53 +0200

    Add action to edit user

--- src/store/actions/usersActions.js
+++ src/store/actions/usersActions.js
@@ -1,5 +1,7 @@
 export const GET_USERS = 'GET_USERS'
 export const FETCH_USER_LIST_SUCCESS = 'FETCH_USER_LIST_SUCCESS'
 export const FETCH_USER_LIST_FAILED = 'FETCH_USER_LIST_FAILED'
+export const UPDATE_USER = 'UPDATE_USER'
+export const UPDATE_USER_SUCCESS = 'UPDATE_USER_SUCCESS'
 
 export const getUsersByPage = (pageNum = 1) => ({ type: GET_USERS, pageNum })
